8b160e7f7cda83dad237818d63fd99a8d0a00bfe
liana.valeria15 <liana.valeria15@gmail.com>
2022-02-20 16:00:42 +0000

    added header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { Header } from "./components/Header";
+
 export const App = () => {
-  return <h1> First recap practice</h1>;
+  return <Header title="UUID Generator" subtitle="Online UUID generator" />;
 };
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,8 @@
+export const Header = ({ title, subtitle }) => {
+  return (
+    <header>
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
+    </header>
+  );
+};
