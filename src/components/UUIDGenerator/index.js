72650e78a38f10775604f04cf5934f1bc8f564c1
liana.valeria15 <liana.valeria15@gmail.com>
2022-02-20 16:52:18 +0000

    added event on btn and declared callback fs

--- src/components/UUIDGenerator/index.js
+++ src/components/UUIDGenerator/index.js
@@ -1,13 +1,19 @@
 import "./UUIDGenerator.css";
 
 export const UUIDGenerator = () => {
+  const generateUUID = () => {
+    console.log("click");
+  };
+
   return (
     <section className="uuid-container">
       <div className="uuid-item">
-        <button className="btn">Generate UUID</button>
+        <button onClick={generateUUID} className="btn">
+          Generate UUID
+        </button>
       </div>
       <div className="uuid-item uuid-text-block">
-        <h3 className="uuid-text">UUID goes here</h3>
+        <h3 className="uuid-text">Generated UUID will be displayed here</h3>
       </div>
     </section>
   );
